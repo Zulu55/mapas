081416c1c4121018b635610f7c674741201451cb
Zulu <jczuluaga@copaair.com>
2018-10-17 16:12:21 -0500

    Works on iOS

--- mapas.Android/MainActivity.cs
+++ mapas.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.CurrentActivity;
 
 namespace mapas.Droid
 {
@@ -19,7 +20,15 @@ namespace mapas.Droid
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.FormsMaps.Init(this, savedInstanceState);
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
             LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
--- mapas/MainPage.xaml.cs
+++ mapas/MainPage.xaml.cs
@@ -1,17 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
-
-namespace mapas
+﻿namespace mapas
 {
+    using System;
+    using Plugin.Geolocator;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Maps;
+
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            this.Locator();
+        }
+
+        private async void Locator()
+        {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 50;
+
+            var location = await locator.GetPositionAsync();
+            var position = new Position(location.Latitude, location.Longitude);
+            this.MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(position, Distance.FromKilometers(1)));
+        }
+
+        private void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            var zoomLevel = double.Parse(e.NewValue.ToString()) * 18.0;
+            var latlongdegrees = 360 / (Math.Pow(2, zoomLevel));
+            this.MyMap.MoveToRegion(new MapSpan(this.MyMap.VisibleRegion.Center, latlongdegrees, latlongdegrees));
         }
     }
 }
